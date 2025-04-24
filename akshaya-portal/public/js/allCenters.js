e71826a57aab6dd94efcea0f55b291d0f5492544
TechnoTOG <xaviererick879@gmail.com>
2025-04-24 09:41:40 +0530

    changed profile function to another js for reusability

--- akshaya-portal/public/js/allCenters.js
+++ akshaya-portal/public/js/allCenters.js
@@ -67,15 +67,3 @@ function filterCentres() {
     }
   });
 }
-
-async function redirect(direction) {
-  if(direction === 'logout'){
-    window.location.href = '/logout';
-  } else if (direction === 'profile'){
-    window.location.href = '/profile';
-  } else if (direction === 'change-pass'){
-    window.location.href = '/change-password';
-  } else {
-    console.error("Invalid Redirection!");
-  }
-}
