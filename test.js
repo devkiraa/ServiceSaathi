dc5fd827f4fa24f8d83edf35ddf0f01e069daaee
devkiraa <devkiraa@gmail.com>
2025-04-27 10:52:54 +0530

    chore: remove unused password hashing code from test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const password = 'admin';
-const saltRounds = 10;
-
-bcrypt.hash(password, saltRounds, function(err, hash) {
-  if (err) throw err;
-  console.log('Hashed password:', hash);
-});
