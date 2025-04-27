d12307cc8b5ef0d5baa45804284ebacc181e29b3
devkiraa <devkiraa@gmail.com>
2025-04-27 19:44:56 +0530

    Enhance logging with chalk for better visibility and add status monitor for network connectivity checks

--- WhatsAppBotServer/config/db.js
+++ WhatsAppBotServer/config/db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+const chalk = require('chalk'); // Import chalk
 
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log("✅ MongoDB Atlas connected to CHATBOTDB");
         console.log('\n==================================');
-        console.log('\nLogs from Service Saathi WhatsApp Server');
+        console.log(`${chalk.magenta('\nLogs from Service Saathi WhatsApp Server')}`);
     } catch (error) {
         console.error("❌ MongoDB connection error:", error);
         process.exit(1);
